11d7e13efcbce8532c0ea4441926996f944c341c
Jake Slattery <alexander.slattery@googlemail.com>
2024-05-29 12:00:09 +0100

    added endpoint for articles and updated MVC inline with review

---
+++ models/articlesById.models.js
@@ -0,0 +1,22 @@
+const db = require("../db/connection");
+
+exports.findArticle = (article_id) => {
+  let queryString = ` SELECT articles.* FROM articles `;
+  const queries = [];
+
+  if (article_id) {
+    queryString += ` WHERE article_id = $1`;
+    queries.push(article_id);
+  }
+  console.log(queryString, queries);
+  return db.query(queryString, queries).then((query) => {
+    if (query.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: `ERROR: no article with that id found`,
+      });
+    } else {
+      return query.rows;
+    }
+  });
+};
