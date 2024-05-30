d68a26b1f26e3de6914f3c2c362fab55544b7529
Jake Slattery <alexander.slattery@googlemail.com>
2024-05-30 10:25:45 +0100

    added post request for comments

--- controllers/articleComments.controllers.js
+++ controllers/articleComments.controllers.js
@@ -1,13 +1,25 @@
-const { findArticleComments } = require("../models/articleComments.models")
-
+const { findArticleComments } = require("../models/articleComments.models");
+const { createArticleComments } = require("../models/articlesById.models");
 
 exports.getArticleComments = (req, res, next) => {
-    const{article_id} = req.params
-    return findArticleComments(article_id)
+  const { article_id } = req.params;
+  return findArticleComments(article_id)
     .then((comments) => {
-        res.status(200).send({comments})
+      res.status(200).send({ comments });
     })
     .catch((err) => {
-      next(err)
+      next(err);
+    });
+};
+
+exports.postArticleComment = (req, res, next) => {
+  const { article_id } = req.params;
+  const { body } = req;
+  return createArticleComments(article_id, body)
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
--- models/articlesById.models.js
+++ models/articlesById.models.js
@@ -2,13 +2,13 @@ const db = require("../db/connection");
 
 exports.findArticle = (article_id) => {
   let queryString = ` SELECT articles.* FROM articles `;
-  const queries = [];
+  const articleId = [];
 
   if (article_id) {
     queryString += ` WHERE article_id = $1`;
-    queries.push(article_id);
+    articleId.push(article_id);
   }
-  return db.query(queryString, queries).then((query) => {
+  return db.query(queryString, articleId).then((query) => {
     if (query.rows.length === 0) {
       return Promise.reject({
         status: 404,
@@ -18,4 +18,28 @@ exports.findArticle = (article_id) => {
       return query.rows;
     }
   });
+};
+
+exports.createArticleComments = (article_id, body) => {
+  commentBody = [article_id, body.author, body.body];
+
+  return db
+    .query(
+      `INSERT INTO comments 
+    (article_id, author, body) 
+    VALUES 
+    ($1, $2, $3) 
+    returning *`,
+      commentBody
+    )
+    .then((query) => {
+      if (query.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `ERROR: no article with that id found`,
+        });
+      } else {
+        return query.rows;
+      }
+    });
 };
